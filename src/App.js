6b21b43b4ec79faef92939753551264c44277d54
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-12-29 09:35:56 -0600

    started adding an app bar to my website first

--- src/App.js
+++ src/App.js
@@ -7,38 +7,12 @@ import ContactForm from './components/Contact'
 import Resume from './components/Resume'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
 function App() {
-
-  const [contactSelected, setContactSelected] = useState(false);
-  const [portfolioSelected, setPortfolioSelected] = useState(false);
-  const [aboutmeSelected, setAboutmeSelected] = useState(true);
-  const [resumeSelected, setResumeSelected] = useState(false);
-
-  const [categories] = useState([
-    {
-      name: "portfolio",
-      description:
-        "Projects & Work",
-    }
-  ]);
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
   return (
     <Router>
-      <div>
-        <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-        portfolioSelected={portfolioSelected}
-        setPortfolioSelected={setPortfolioSelected}
-        aboutmeSelected={aboutmeSelected}
-        setAboutmeSelected={setAboutmeSelected}
-        resumeSelected={resumeSelected}
-        setResumeSelected={setResumeSelected}
-        ></Nav>
+      <div className="App">
+        <Nav></Nav>
         <main>
           <Switch>
             <Route path="/" exact component={About}/>
@@ -47,34 +21,6 @@ function App() {
             <Route path="/contact" component={ContactForm}/>
             <Route path="/resume" component={Resume}/>
           </Switch>
-        {/* {!contactSelected ? (
-            <>
-              
-            </>
-          ) : (
-              <ContactForm></ContactForm>
-            )} 
-        {!portfolioSelected ? (
-            <>
-              
-            </>
-          ) : (
-            <Project currentCategory={currentCategory}></Project>
-            )}
-        {!aboutmeSelected ? (
-            <>
-              
-            </>
-          ) : (
-              <About></About>
-            )}
-        {!resumeSelected ? (
-            <>
-              
-            </>
-          ) : (
-              <Resume></Resume>
-            )} */}
         </main>
         <Footer></Footer>
       </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,130 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from "../../utils/helpers";
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-function Nav() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <header className="flex-row">
-        <h2 className="mx-3 my-2">
-            <Link to='/'>
-                <a>Saul Huerta</a>
-            </Link>
-        </h2>
-        <nav>
-            <ul className="flex-row my-8">
-                <Link to='/about'>
-                    <li><span>About Me</span></li>
-                </Link>
-                <Link to='/portafolio'>
-                    <li><span>Portafolio</span></li>
-                </Link>
-                <Link to='/contact'>
-                    <li><span>Contact</span></li>
-                </Link>
-                <Link to='/resume'>
-                    <li><span>Resume</span></li>
-                </Link>
-            </ul>
-        </nav>
-    </header>
+    <div className={classes.root}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
-export default Nav;
+// import React, { useEffect } from 'react';
+// import { capitalizeFirstLetter } from "../../utils/helpers";
+// import { Link } from 'react-router-dom';
+
+// function Nav() {
+//   return (
+//     <header className="flex-row">
+//         <h2 className="mx-3 my-2">
+//             <Link to='/'>
+//                 <a>Saul Huerta</a>
+//             </Link>
+//         </h2>
+//         <nav>
+//             <ul className="flex-row my-8">
+//                 <Link to='/about'>
+//                     <li><span>About Me</span></li>
+//                 </Link>
+//                 <Link to='/portafolio'>
+//                     <li><span>Portafolio</span></li>
+//                 </Link>
+//                 <Link to='/contact'>
+//                     <li><span>Contact</span></li>
+//                 </Link>
+//                 <Link to='/resume'>
+//                     <li><span>Resume</span></li>
+//                 </Link>
+//             </ul>
+//         </nav>
+//     </header>
+//   );
+// }
+
+// export default Nav;
