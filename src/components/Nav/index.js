c526453a9175a93e0fe68c94c07243eb7d5ca06b
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-12-31 11:21:16 -0600

    added some cleaner code to the nav and made it more simple to call the routes for buttons and menu items

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -33,108 +33,98 @@ const useStyles = makeStyles((theme) => ({
 
 const Nav = (props) => {
     const { history } = props;
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleMenuClick = (pageURL) => {
-      history.push(pageURL);
-    setAnchorEl(null);
-  };
-
-  const handleButtonClick = (pageURL) => {
+    const handleMenuClick = (pageURL) => {
         history.push(pageURL);
-  };
+        setAnchorEl(null);
+    };
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Saul Huerta
-          </Typography>
-              {isMobile ? (
-                <>
-                  <IconButton 
-                  edge="start" 
-                  className={classes.menuButton} 
-                  color="inherit"
-                  onClick={handleMenu} 
-                  aria-label="menu">
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={() => setAnchorEl(null)}
-                >
-                  <MenuItem onClick={() => handleMenuClick('/about')}>About Me</MenuItem>
-                  <MenuItem onClick={() => handleMenuClick('/portfolio')}>Portfolio</MenuItem>
-                  <MenuItem onClick={() => handleMenuClick('/resume')}>Resume</MenuItem>
-                  <MenuItem onClick={() => handleMenuClick('/contact')}>Contact Me</MenuItem>
-                </Menu>
-                </>
-                ): (
-                    <div className={classes.NavOptions}>
-                        <Button variant="contained" onClick={() => handleButtonClick('/about')}>About Me</Button>
-                        <Button variant="contained" onClick={() => handleButtonClick('/portfolio')}>Portfolio</Button>
-                        <Button variant="contained" onClick={() => handleButtonClick('/resume')}>Resume</Button>
-                        <Button variant="contained" onClick={() => handleButtonClick('/contact')}>Contact</Button>
-                    </div>
-                )}
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    const handleButtonClick = (pageURL) => {
+        history.push(pageURL);
+    };
+
+    const menuItems = [
+        {
+            menuTitle: 'About',
+            pageURL: '/about',
+        },
+        {
+            menuTitle: 'Portfolio',
+            pageURL: '/portfolio',
+        },
+        {
+            menuTitle: 'Resume',
+            pageURL: '/resume',
+        },
+        {
+            menuTitle: 'Contact',
+            pageURL: '/contact',
+        },
+    ];
+
+    return (
+        <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+                Saul Huerta
+            </Typography>
+                {isMobile ? (
+                    <>
+                    <IconButton 
+                    edge="start" 
+                    className={classes.menuButton} 
+                    color="inherit"
+                    onClick={handleMenu} 
+                    aria-label="menu">
+                    <MenuIcon />
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={() => setAnchorEl(null)}
+                    >
+                        {menuItems.map(menuItem => {
+                            const {menuTitle, pageURL} = menuItem;
+                            return (
+                                <MenuItem onClick={() => handleMenuClick(pageURL)}>{menuTitle}</MenuItem>
+                            )
+                        })}
+                    </Menu>
+                    </>
+                    ): (
+                        <div className={classes.NavOptions}>
+                            {menuItems.map(menuItem => {
+                            const {menuTitle, pageURL} = menuItem;
+                            return (
+                                <Button variant="contained" onClick={() => handleButtonClick(pageURL)}>{menuTitle}</Button>
+                            )
+                            })}
+                        </div>
+                    )}
+            </Toolbar>
+        </AppBar>
+        </div>
+    );
 }
 
 export default withRouter(Nav);
-// import React, { useEffect } from 'react';
-// import { capitalizeFirstLetter } from "../../utils/helpers";
-// import { Link } from 'react-router-dom';
-
-// function Nav() {
-//   return (
-//     <header className="flex-row">
-//         <h2 className="mx-3 my-2">
-//             <Link to='/'>
-//                 <a>Saul Huerta</a>
-//             </Link>
-//         </h2>
-//         <nav>
-//             <ul className="flex-row my-8">
-//                 <Link to='/about'>
-//                     <li><span>About Me</span></li>
-//                 </Link>
-//                 <Link to='/portafolio'>
-//                     <li><span>Portafolio</span></li>
-//                 </Link>
-//                 <Link to='/contact'>
-//                     <li><span>Contact</span></li>
-//                 </Link>
-//                 <Link to='/resume'>
-//                     <li><span>Resume</span></li>
-//                 </Link>
-//             </ul>
-//         </nav>
-//     </header>
-//   );
-// }
-
-// export default Nav;
